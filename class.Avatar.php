5f9f600bb495dcbfd108b595dfe1546f67377430
Devat Karetha <devat.karetha@viitor.cloud>
2019-08-05 15:05:12 +0530

    added source

---
+++ class.Avatar.php
@@ -0,0 +1,86 @@
+<?php
+
+/******************************************************************
+ * 
+ * Projectname:   Avatar Builder  
+ * Version:       1.0
+ * Author:        DEVAT KARETHA <devat.karetha@viitor.cloud>
+ * Last modified: 05 08 2019
+ * Copyright (C): 2019 All Rights Reserved
+ * 
+ * GNU General Public License (Version 2, June 1991)
+ *
+ * This program is free software; you can redistribute
+ * it and/or modify it under the terms of the GNU
+ * General Public License as published by the Free
+ * Software Foundation; either version 2 of the License,
+ * or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will
+ * be useful, but WITHOUT ANY WARRANTY; without even the
+ * implied warranty of MERCHANTABILITY or FITNESS FOR A
+ * PARTICULAR PURPOSE. See the GNU General Public License
+ * for more details.
+ * 
+ ******************************************************************/
+ 
+ 
+class Avatar
+{	
+	public $gender;
+	public $hair;
+	public $nose;
+	public $lips;
+	public $eyes;
+	
+	### this property considered for seperator character in picture names 
+	public $seperator = '-';
+	
+	### this property considered for Location of images
+	public $imageLoc = 'images/';
+	
+	### this is a defualt style for avatars ;) you could change it ;)
+	public $style = 'style="left:23px;top:36px;position:absolute;"';
+	
+	public $output;
+	
+	public function __construct($gender)
+	{
+		$this->gender = $gender;
+		return $this->gender;
+	}
+		
+	public function setHair($type = '1' , $color = 'defualt')
+	{
+		$this->hair = $type.$this->seperator.$color;
+	}
+		
+	public function setNose($type = '1' , $color = 'defualt')
+	{
+		$this->nose = $type.$this->seperator.$color;
+	}
+		
+	public function setLips($type = '1' , $color = 'defualt')
+	{
+		$this->lips = $type.$this->seperator.$color;
+	}
+		
+	public function setEyes($type = '1' , $color = 'defualt')
+	{
+		$this->eyes = $type.$this->seperator.$color;
+	}
+		
+	public function draw()
+	{
+		$this->output  = '<div style="position:relative;width:145px;height:170px;border:solid 1px black;padding:3px;float:left;margin:5px;">';	
+		$this->output .= '<img src="'.$this->imageLoc.$this->gender.$this->seperator.'base.png" '.$this->style.'>';
+		$this->output .= '<img src="'.$this->imageLoc.$this->gender.$this->seperator.'hair'.$this->seperator.$this->hair.'.png" style="position:absolute;">';
+		$this->output .= '<img src="'.$this->imageLoc.$this->gender.$this->seperator.'eyes'.$this->seperator.$this->eyes.'.png" '.$this->style.'>';
+		$this->output .= '<img src="'.$this->imageLoc.$this->gender.$this->seperator.'nose'.$this->seperator.$this->nose.'.png" '.$this->style.'>';
+		$this->output .= '<img src="'.$this->imageLoc.$this->gender.$this->seperator.'lips'.$this->seperator.$this->lips.'.png" '.$this->style.'>';
+		$this->output .= '</div>';
+			
+		return $this->output;
+	}
+		
+}
---
+++ index.php
@@ -0,0 +1,61 @@
+<?php
+
+include './class.Avatar.php';
+
+?>
+
+<!doctype html>
+<html>
+    <head>
+        <meta charset="utf-8">
+        <title>Avatar Builder</title>
+        <link rel="shortcut icon" href="favicon.ico" type="image/x-icon"/>
+    </head>
+
+    <body>
+
+        <?php
+
+            $anna = new Avatar('f');
+            $anna->setHair();
+            $anna->setNose();
+            $anna->setLips();
+            $anna->setEyes();
+            // echo "<pre>";
+            // print_r($anna);
+            // echo "</pre>";
+            // die;
+            echo $anna->draw();
+
+            $sara = new Avatar('f');
+            $sara->setHair('1','black');
+            $sara->setNose('2');
+            $sara->setLips('1','red');
+            $sara->setEyes();
+            echo $sara->draw();
+
+            $elizabeth = new Avatar('f');
+            $elizabeth->setHair('1','brown');
+            $elizabeth->setNose('1');
+            $elizabeth->setLips('1','red');
+            $elizabeth->setEyes();
+            echo $elizabeth->draw();
+
+            $john = new Avatar('m');
+            $john->setHair();
+            $john->setNose();
+            $john->setLips();
+            $john->setEyes();
+            echo $john->draw();
+
+            $jack = new Avatar('m');
+            $jack->setHair('1','brown');
+            $jack->setNose('1');
+            $jack->setLips('1');
+            $jack->setEyes();
+            echo $jack->draw();
+
+        ?>
+
+    </body>
+</html>
